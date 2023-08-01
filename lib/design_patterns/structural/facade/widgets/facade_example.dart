97a3cce5c898dad25ffc89f23f8edafd83b5c572
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2023-08-01 14:51:55 +0300

    implement facade with example

---
+++ lib/design_patterns/structural/facade/widgets/facade_example.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/facade/facades/smart_home_facade.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/facade/smart_home_state.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'device_icon.dart';
+import 'mode_switcher.dart';
+
+class FacadeExample extends StatefulWidget {
+  const FacadeExample();
+
+  @override
+  _FacadeExampleState createState() => _FacadeExampleState();
+}
+
+class _FacadeExampleState extends State<FacadeExample> {
+  final _smartHomeFacade = const SmartHomeFacade();
+  final _smartHomeState = SmartHomeState();
+
+  var _homeCinemaModeOn = false;
+  var _gamingModeOn = false;
+  var _streamingModeOn = false;
+
+  bool get _isAnyModeOn =>
+      _homeCinemaModeOn || _gamingModeOn || _streamingModeOn;
+
+  void _changeHomeCinemaMode(bool activated) {
+    if (activated) {
+      _smartHomeFacade.startMovie(_smartHomeState, 'Movie title');
+    } else {
+      _smartHomeFacade.stopMovie(_smartHomeState);
+    }
+
+    setState(() => _homeCinemaModeOn = activated);
+  }
+
+  void _changeGamingMode(bool activated) {
+    if (activated) {
+      _smartHomeFacade.startGaming(_smartHomeState);
+    } else {
+      _smartHomeFacade.stopGaming(_smartHomeState);
+    }
+
+    setState(() => _gamingModeOn = activated);
+  }
+
+  void _changeStreamingMode(bool activated) {
+    if (activated) {
+      _smartHomeFacade.startStreaming(_smartHomeState);
+    } else {
+      _smartHomeFacade.stopStreaming(_smartHomeState);
+    }
+
+    setState(() => _streamingModeOn = activated);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+      behavior: const ScrollBehavior(),
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(),
+        child: Column(
+          children: <Widget>[
+            ModeSwitcher(
+              title: 'Home cinema mode',
+              activated: _homeCinemaModeOn,
+              onChanged: !_isAnyModeOn || _homeCinemaModeOn
+                  ? _changeHomeCinemaMode
+                  : null,
+            ),
+            ModeSwitcher(
+              title: 'Gaming mode',
+              activated: _gamingModeOn,
+              onChanged:
+                  !_isAnyModeOn || _gamingModeOn ? _changeGamingMode : null,
+            ),
+            ModeSwitcher(
+              title: 'Streaming mode',
+              activated: _streamingModeOn,
+              onChanged: !_isAnyModeOn || _streamingModeOn
+                  ? _changeStreamingMode
+                  : null,
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    DeviceIcon(
+                      iconData: FontAwesomeIcons.tv,
+                      activated: _smartHomeState.tvOn,
+                    ),
+                    DeviceIcon(
+                      iconData: FontAwesomeIcons.film,
+                      activated: _smartHomeState.netflixConnected,
+                    ),
+                    DeviceIcon(
+                      iconData: Icons.speaker,
+                      activated: _smartHomeState.audioSystemOn,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    DeviceIcon(
+                      iconData: FontAwesomeIcons.playstation,
+                      activated: _smartHomeState.gamingConsoleOn,
+                    ),
+                    DeviceIcon(
+                      iconData: FontAwesomeIcons.video,
+                      activated: _smartHomeState.streamingCameraOn,
+                    ),
+                    DeviceIcon(
+                      iconData: FontAwesomeIcons.lightbulb,
+                      activated: _smartHomeState.lightsOn,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
