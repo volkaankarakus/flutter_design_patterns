d40353af7201115e0e4a470ba9c3f2683628a761
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2023-08-03 13:32:00 +0300

    implement flyweight widgets

---
+++ lib/design_patterns/structural/flyweight/widgets/flyweight_example.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/flyweight/ipositioned_shape.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/flyweight/shape_factory.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/flyweight/shape_flyweight_factory.dart';
+import 'package:flutter_design_patterns/design_patterns/structural/flyweight/shape_type_enum.dart';
+
+import 'positioned_shape_wrapper.dart';
+
+class FlyweightExample extends StatefulWidget {
+  const FlyweightExample();
+
+  @override
+  _FlyweightExampleState createState() => _FlyweightExampleState();
+}
+
+class _FlyweightExampleState extends State<FlyweightExample> {
+  static const shapesCount = 1000;
+
+  final shapeFactory = const ShapeFactory();
+
+  late final ShapeFlyWeightFactory _shapeFlyweightFactory;
+  late List<IPositinedShape> _shapesList;
+
+  var _shapeInstancesCount = 0;
+  var _useFlyweightFactory = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _shapeFlyweightFactory = ShapeFlyWeightFactory(shapeFactory: shapeFactory);
+
+    _buildShapesList();
+  }
+
+  void _buildShapesList() {
+    var shapeInstancesCount = 0;
+    _shapesList = <IPositinedShape>[];
+
+    for (var i = 0; i < shapesCount; i++) {
+      final shapeType = _getRandomShapeType();
+      final shape = _useFlyweightFactory
+          ? _shapeFlyweightFactory.getShape(shapeType)
+          : shapeFactory.createShape(shapeType);
+
+      shapeInstancesCount++;
+      _shapesList.add(shape);
+    }
+
+    setState(() {
+      _shapeInstancesCount = _useFlyweightFactory
+          ? _shapeFlyweightFactory.getShapeInstancesCount()
+          : shapeInstancesCount;
+    });
+  }
+
+  ShapeType _getRandomShapeType() {
+    const values = ShapeType.values;
+
+    return values[Random().nextInt(values.length)];
+  }
+
+  void _toggleUseFlyweightFactory(bool value) {
+    setState(() => _useFlyweightFactory = value);
+
+    _buildShapesList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        for (var shape in _shapesList)
+          PositionedShapeWrapper(
+            shape: shape,
+          ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SwitchListTile.adaptive(
+              title: const Text(
+                'Use flyweight factory',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              activeColor: Colors.black,
+              value: _useFlyweightFactory,
+              onChanged: _toggleUseFlyweightFactory,
+            ),
+          ],
+        ),
+        Center(
+          child: Text(
+            'Shape instances count: $_shapeInstancesCount',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
